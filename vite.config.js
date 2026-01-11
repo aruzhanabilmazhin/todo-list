c92d1d3771f335177217c8499858a9e7a9c9e6ed
aruzhana <aruzhana@byui.edu>
2026-01-11 17:30:54 +0500

    ииии

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/todo-list/' // 
-})
+  base: '/todo-list/' // важная настройка для GitHub Pages
+});
